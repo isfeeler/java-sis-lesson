d9b7fde17592718d888f5dff1cda8995525837f6
Svatko Igor <isfeeler@gmail.com>
2020-02-08 19:28:04 +0700

    initial commit

---
+++ 1-LESSON/AnotherClass.java
@@ -0,0 +1,9 @@
+public class AnotherClass {
+	public static String getName() {
+		return "Igor";
+	}
+
+	public static int getAge() {
+		return 10;
+	}
+}
---
+++ 1-LESSON/MyApplication.java
@@ -0,0 +1,11 @@
+public class MyApplication {
+	public static void main(String[] args) {
+		System.out.println("Hello world!");
+
+		String name = AnotherClass.getName();
+		System.out.println("Name = "+name);
+
+		// int age = AnotherClass.getAge();
+		// System.out.println("Age = "+age);
+	}
+}
